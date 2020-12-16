442146257297400e478491a8583e688ba200410c
Arunav Khare <arunav.khare@gmail.com>
2020-12-16 18:48:37 +0530

    Fixed /docs path

--- docs/js/common.js
+++ docs/js/common.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
     // URLs for menu
-    var URL_MENU   = document.location.origin + '/_shared/toc.menu';
+    var URL_MENU   = document.location.origin + '/jan2021/_shared/toc.menu';
 
     // --------------------------------------------
     //  A S Y N C    W O R K E R S
@@ -85,7 +85,7 @@ $(document).ready(function () {
 
     // gets a link based on hyperlink name
     window.getLink = function(hyperlinkTxt) {
-        return document.location.origin + '/pages/article.html?title=' + spaceSepTxtToHypenSep(hyperlinkTxt);
+        return document.location.origin + '/jan2021/pages/article.html?title=' + spaceSepTxtToHypenSep(hyperlinkTxt);
     };
 
     // get all lnks in menu
@@ -100,7 +100,7 @@ $(document).ready(function () {
 
     // gets markdown page url for current invoked page
     window.getMarkdownFileURL = function() {
-        return document.location.origin + '/pages/mds/' + getCurrentPage() + '.md';
+        return document.location.origin + '/jan2021/pages/mds/' + getCurrentPage() + '.md';
     };
 
     // finds in menu item with given title
@@ -139,7 +139,7 @@ $(document).ready(function () {
 
     // returns the image URL for given user
     window.getUserImageLink= function(name) {
-        return document.location.origin + '/pages/images/authors/' + spaceSepTxtToHypenSep(name) + ".jpg";
+        return document.location.origin + '/jan2021/pages/images/authors/' + spaceSepTxtToHypenSep(name) + ".jpg";
     };
 
     // sets previous and next buttons hrefs
